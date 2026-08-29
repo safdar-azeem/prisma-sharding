faeb31b46dbba6cdbfd472f6320e9ecb3b72ac2a
safdar-azeem <safdarazeem00@gmail.com>
2026-08-29 07:35:23 +0500

    Update user.routes.ts

--- examples/example-1/src/routes/user.routes.ts
+++ examples/example-1/src/routes/user.routes.ts
@@ -4,4 +4,4 @@ import { UserController } from '../controllers/user.controller.js';
 export const userRoutes = Router();
 
 userRoutes.post('/', UserController.create);
-userRoutes.get('/:id', UserController.getById);
+userRoutes.get('/me', UserController.me);
